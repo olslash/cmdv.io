ab3cb35a790df030b79a240baaab1f0ba5b4c701
Mitch R <olordhux+github@gmail.com>
2015-01-18 03:37:36 -0800

    remove logging

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,5 @@ var express = require('express');
 module.exports = indexRouter = express.Router();
 
 indexRouter.get('/:key?', function (req, res) {
-  console.log('here');
   res.render('index')
 });
