63ad8cf850ab4c76902064362ca57ff844fbebca
Mitch R <olordhux+github@gmail.com>
2015-01-31 13:03:41 -0800

    combine POST / and POST /:key into one route; improve server error handling

--- server/controllers/pastes.js
+++ server/controllers/pastes.js
@@ -6,24 +6,7 @@ var db = require('../db'), // fixme: don't pretend this is sync
 
 module.exports =  pasteRouter = express.Router();
 
-//pasteRouter.param('key', /^[a-zA-Z]+$/);
 pasteRouter.use(bodyParser.text());
-
-//pasteRouter.get('/test', function (req, res) {
-////  db.getRevisionHistoryForKey('ragupuyo').then(function(key) {
-////    console.log('result');
-////    console.log(key);
-////  }).catch(function(err) {
-////    console.log(err);
-////  });
-////  db.retrievePaste('sajifose').then(function(result) {
-////    console.log(result);
-////  })
-//  db.savePaste('content', 'ragupuyo');
-////  db.generateUniqueKey(8).then(function(key) {
-////    console.log(key);
-////  })
-//});
 
 pasteRouter.get('/:key', function (req, res) {
   var key = req.params.key.toLowerCase();
@@ -35,31 +18,23 @@ pasteRouter.get('/:key', function (req, res) {
   });
 });
 
-// save a paste, unrelated to any other
-pasteRouter.post('/', function (req, res) {
+pasteRouter.post('/:key?', function (req, res) {
+  // 'key' is the optional existing key to associate this new revision with
   // returns
   // pasteContent: the content just saved
   // pasteID: the ID of the paste just saved
   // revisions: array of revisions related to this ID
 
-  var content = req.body;
-  if(content.length === 0) return res.status(400).end();
+  var key = req.params.key;
+  if(key) key = key.toLowerCase();
 
-  db.savePaste(content).then(function(data) {
-    res.json(data);
-  }).catch(function(err) {
-    res.status(404).json({error: err});
-  });
-});
-
-pasteRouter.post('/:key', function (req, res) {
-  // 'key' is the existing key to associate this new revision with
-  var key = req.params.key.toLowerCase();
   var content = req.body;
-  if (content.length === 0) return res.status(400).end();
+  if (content.length === 0) return res.status(400).send({error: 'cannot save paste of zero length'});
 
   db.savePaste(content, key).then(function (data) {
     res.json(data);
+  }).catch(function(err) {
+    res.status(500).json({error: err});
   })
 });
 
