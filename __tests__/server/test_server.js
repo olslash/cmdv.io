1942426352a287068863c5d090eaea722df89bad
Mitch R <olordhux+github@gmail.com>
2015-02-06 22:36:38 -0800

    tests for server revision chaining behavior

--- __tests__/server/test_server.js
+++ __tests__/server/test_server.js
@@ -46,10 +46,10 @@ appPromise.then(function(app) {
 
     it('should re-generate keys that are not unique', function(done) {
       var helpers = require('../../helpers');
-      sinon.stub(helpers, 'generateKey').onCall(0).returns('himom')
-                                        .onCall(1).returns('himom')
-                                        .onCall(2).returns('himom')
-                                        .onCall(3).returns('byemom');
+      var stub = sinon.stub(helpers, 'generateKey').onCall(0).returns('himom')
+                                                   .onCall(1).returns('himom')
+                                                   .onCall(2).returns('himom')
+                                                   .onCall(3).returns('byemom');
 
       // save a paste with key 'himom'
       request(app)
@@ -58,9 +58,8 @@ appPromise.then(function(app) {
         .send('this is a paste!')
         .expect('Content-Type', /json/)
         .expect(function (res) {
-            console.log(res.body);
-            var assignedPasteID = res.body.pasteID;
-            assignedPasteID.should.equal('himom');
+          var assignedPasteID = res.body.pasteID;
+          assignedPasteID.should.equal('himom');
         })
           // force it to try and save another paste with that key-- it should
           // call generateKey until it finds a keyi that is not unique
@@ -70,14 +69,75 @@ appPromise.then(function(app) {
             .type('text/plain')
             .send('this is a paste!')
             .expect(function (res) {
-                console.log(res.body);
               var assignedPasteID = res.body.pasteID;
               assignedPasteID.should.equal('byemom');
             })
-            .end(done);
+            .end(function(err) {
+                helpers.generateKey.restore();
+                done(err);
+              });
         });
-//        .end(done)
+    });
 
+    it('should create a single-entry revision chain for new pastes', function(done) {
+      request(app)
+        .post('/pastes')
+        .type('text/plain')
+        .send('this is a paste!')
+        .end(function (err, res) {
+            res.body.revisions.length.should.equal(1);
+            res.body.revisions.should.containEql(res.body.pasteID);
+            done(err)
+          });
+    });
+
+    it('should append to the revision chain when saving a paste to an existing ID', function(done) {
+      // save a paste
+      request(app)
+        .post('/pastes')
+        .type('text/plain')
+        .send('this is a paste!')
+
+      // save a new paste as a revision of the first
+        .end(function(err, res) {
+          var firstResponse = res.body;
+          request(app)
+            .post('/pastes/' + firstResponse.pasteID)
+            .type('text/plain')
+            .send('this is another paste!')
+            .end(function (err, res) {
+                var secondResponse = res.body;
+                var firstKey = firstResponse.pasteID;
+                var secondKey = secondResponse.pasteID;
+
+                firstKey.should.not.equal(secondKey);
+
+                firstResponse.pasteContent.should.equal('this is a paste!');
+                secondResponse.pasteContent.should.equal('this is another paste!');
+                secondResponse.revisions.length.should.equal(2);
+                secondResponse.revisions.should.containEql(firstKey);
+
+                request(app)
+      // save a paste to the second paste
+                  .post('/pastes/' + secondResponse.pasteID)
+                  .type('text/plain')
+                  .send('this is a third paste!')
+                  .end(function(err, res) {
+                      var thirdKey = res.body.pasteID;
+                      res.body.revisions.length.should.equal(3);
+      // retrieve the first paste and make sure everything is there
+                      request(app)
+                        .get('/pastes/' + firstKey)
+                        .end(function(err, res){
+                            res.body.revisions.should.containEql(firstKey);
+                            res.body.revisions.should.containEql(secondKey);
+                            res.body.revisions.should.containEql(thirdKey);
+
+                            done(err);
+                          });
+                    });
+            });
+        });
     })
   });
 
