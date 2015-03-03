d3922eae951d1a5dd808dcb35a4df1764989a322
Mitch R <olordhux+github@gmail.com>
2015-03-02 23:06:23 -0800

    /raw route for plaintext paste content

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,22 @@
 var express = require('express'),
     React   = require('react');
 
-var Index = require('../../frontend/Index.jsx');
+var Index = require('../../frontend/Index.jsx'),
+    db = require('../db');
 
 var indexRouter;
 module.exports = indexRouter = express.Router();
 
 indexRouter.get('/:key?', function (req, res) {
   res.send(React.renderToString(React.createElement(Index)))
+});
+
+// /raw/:key(.extension)
+indexRouter.get(/^\/raw\/(\w+).?(.+)?$/, function (req,res) {
+  res.set('Content-Type', 'text/plain');
+  db.retrievePaste(req.params[0]).then(function (result) {
+    res.send(result.pasteContent);
+  }).catch(function (err) {
+    res.status(404).send(err);
+  });
 });
